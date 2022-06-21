f284332771c71a09e33c701f3d0d73054055e17a
justsevrage <savin@angelmario.eu>
2022-06-21 13:38:09 +0000

    Lint fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,26 +3,26 @@ import { getPerformance } from "firebase/performance";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDxDQKoKzXLhWnqowxQKENGKHLTUdKNf7I",
-    authDomain: "paquetromania.firebaseapp.com",
-    projectId: "paquetromania",
-    storageBucket: "paquetromania.appspot.com",
-    messagingSenderId: "438836893082",
-    appId: "1:438836893082:web:ffe467c2087768ad6f9623",
-    measurementId: "G-3VH95LD4JB"
-}
+	apiKey: "AIzaSyDxDQKoKzXLhWnqowxQKENGKHLTUdKNf7I",
+	authDomain: "paquetromania.firebaseapp.com",
+	projectId: "paquetromania",
+	storageBucket: "paquetromania.appspot.com",
+	messagingSenderId: "438836893082",
+	appId: "1:438836893082:web:ffe467c2087768ad6f9623",
+	measurementId: "G-3VH95LD4JB"
+};
 
 const firebase = initializeApp(firebaseConfig);
 let analytics = null;
 let performance = null;
 
 if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
-    performance = getPerformance(firebase);
-    analytics = getAnalytics(firebase);
+	performance = getPerformance(firebase);
+	analytics = getAnalytics(firebase);
 }
 
 export {
-    firebase,
-    analytics,
-    performance
-}
+	firebase,
+	analytics,
+	performance
+};
