408d10d405c789c61231fd8a53da4d6df7807a7f
justsevrage <savin@angelmario.eu>
2022-06-23 10:57:05 +0000

    Adaugat schimbare de culoare de tema dinamic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ParallaxProvider } from "react-scroll-parallax";
-import { CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
+import { CssBaseline, ThemeProvider, useMediaQuery, useTheme } from "@mui/material";
 import { darkTheme, lightTheme } from "../lib/theme";
 import "../lib/firebase";
 
 
 const App = ({ Component, pageProps }: AppProps) => {
+	const theme = useTheme()
 	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
 	return (
@@ -15,7 +16,6 @@ const App = ({ Component, pageProps }: AppProps) => {
 			<Head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
-				<meta name="theme-color" content="#8267be" />
 				<meta name="description" content="Aducem comunitatea mai aproape" />
 				<link rel="apple-touch-icon" href="/icon.png" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
@@ -24,6 +24,17 @@ const App = ({ Component, pageProps }: AppProps) => {
 				<link rel="manifest" href="/manifest.json"/>
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#8267be"/>
 				<meta name="msapplication-TileColor" content="#8267be"/>
+
+				<meta
+					name="theme-color" 
+					content={
+						prefersDarkMode ? 
+							darkTheme.palette.background.default
+							:
+							lightTheme.palette.background.default
+					}
+				/>
+
 				<title>Paquet Hub</title>
 			</Head>
 			<ThemeProvider theme={prefersDarkMode ? darkTheme : lightTheme}>
