d028e02cc6a23a90c00eb36d701b46c5db588656
notangelmario <savin@angelmario.eu>
2022-06-28 12:40:22 +0300

    Lint fix

--- src/pages/auth/[...supabase].ts
+++ src/pages/auth/[...supabase].ts
@@ -1,3 +1,3 @@
-import { handleAuth } from '@supabase/auth-helpers-nextjs';
+import { handleAuth } from "@supabase/auth-helpers-nextjs";
 
-export default handleAuth({ logout: { returnTo: '/' } });
+export default handleAuth({ logout: { returnTo: "/" } });
