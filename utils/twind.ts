bd54544026ffa2996dc78a997618adf17c2cac5d
notangelmario <savin@angelmario.eu>
2022-06-29 19:02:17 +0300

    Little changes

--- utils/twind.ts
+++ utils/twind.ts
@@ -2,7 +2,7 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
 export * from "twind";
 export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
+	darkMode: "class",
+	mode: "silent",
 };
 if (IS_BROWSER) setup(config);
