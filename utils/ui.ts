a6598fe9cd3513000efeba8ceefa32a0871ad22f
Angel Mario <savin@angelmario.eu>
2022-07-01 20:56:01 +0000

    Added button component Remade button animations Added categories Updated app page

--- utils/ui.ts
+++ utils/ui.ts
@@ -3,16 +3,16 @@ import { css } from "twind/css";
 
 export const btn = apply`
 	  	cursor-pointer
-		scale(hover:[1.01] focus:[1.01] active:[0.99])
-		transition-transform duration-75 ease-in-out origin-center
+		opacity(hover:90 active:80)
+		transition-opacity duration-75 ease-in-out origin-center
 `
 
 export const iconBtn = apply`
 		cursor-pointer 
 		block p-2
 		focus:!outline-0
-		scale(hover:[1.01] focus:[1.01] active:[0.99])
-		transition-transform duration-75 ease-in-out origin-center
+		opacity(hover:90 active:80)
+		transition-opacity duration-150 ease-in-out origin-center
 		${css({
 			"& > span": {
 				display: "block",
