5338d2b3aa80ea9987144689120722e640bbd8e6
notangelmario <savin@angelmario.eu>
2022-07-16 13:27:10 +0300

    feat: added service account supabase

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,8 +3,14 @@ import { createClient } from "supabase";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL") || "";
 const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") || "";
+const SUPABASE_SERVICE_KEY = Deno.env.get("SUPABASE_SERVICE_KEY") || "";
 
 export const supabase = createClient(
 	SUPABASE_URL,
 	SUPABASE_ANON_KEY,
 );
+
+export const supabaseService = createClient(
+	SUPABASE_URL,
+	SUPABASE_SERVICE_KEY
+)
