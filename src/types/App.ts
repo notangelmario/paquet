e7a192c80b2a3eec0797a6dbb8db7e92d0c9a68d
Angel Mario <savin@angelmario.eu>
2022-07-22 00:18:12 +0300

    Updated App type definition

--- src/types/App.ts
+++ src/types/App.ts
@@ -1,3 +1,29 @@
+/**
+ * App type definition
+ * @typedef {Object} App
+ * 
+ * @prop {string} id							The id of the app
+ * @prop {string} name							The name of the app
+ * @prop {string} author						Author name
+ * @prop {string} url							URL pointing to the web app
+ * @prop {string} iconLarge						URL pointing to a large app icon (512x512)
+ * @prop {string} iconSmall						URL pointing to a small app icon (128x128)
+ * @prop {string} categoryId					ID of the category this web app belongs to
+ * @prop {string} description					Web app description
+ * 
+ * @prop {Object} [tags]						Defines aditional tags to the web app
+ * @prop {boolean} [tags.desktop]				Is desktop optimized	
+ * @prop {boolean} [tags.mobile]				Is mobile optimized	
+ * @prop {boolean} [tags.offline]				Does it work offline	
+ * @prop {boolean} [desktop]					Is open source	
+ * 
+ * @prop {Object} [lighthouse]					Displays lighthouse information
+ * @prop {number} lighthouse.performance		Performance score
+ * @prop {number} lighthouse.accessibility		Accessibility score
+ * @prop {number} lighthouse.bestPractices		Best Practices score
+ * @prop {number} lighthouse.seo				SEO score
+ * @prop {number} lighthouse.updatedAt			When was the last time this information was updated at
+ */
 export type App = {
     id: string,
 
@@ -6,21 +32,29 @@ export type App = {
     url: string,
     iconLarge: string, 
     iconSmall: string,
-    /**@deprecated This is no longer used */
+    /**@deprecated Use iconLarge and iconSmall instead */
     iconUrl?: string,
+
     categoryId: string,
     description: string,
 
-    // Not fully implemented yet
+    tags?: {
+        desktop?: boolean,
+        mobile?: boolean,
+        offline?: boolean,
+        openSource?: boolean
+    }
+
     lighthouse?: {
         performance: number,
         accessibility: number,
-        bestPractice: number,
+        bestPractices: number,
         seo: number,
         updatedAt: number
     },    
 
-    // DEPRECATED. To be removed
+    /** @deprecated To be removed*/ 
     appStoreLink?: string,
+    /** @deprecated To be removed*/ 
     playStoreLink?: string
 }
