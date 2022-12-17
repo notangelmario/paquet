fb8105778e9ca66850ab3cf5cfe7fac46dd46cfe
Savin Angel-Mario <savin@angelmario.eu>
2022-12-17 15:47:54 +0200

    Removed prefetching

--- main.ts
+++ main.ts
@@ -8,15 +8,10 @@ import manifest from "@/fresh.gen.ts";
 
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "@/twind.config.ts";
-import prefetchPlugin from "prefetch";
-
 
 await start(manifest, {
 	plugins: [
 		twindPlugin(twindConfig),
-		prefetchPlugin({
-			limit: 4,
-		})
 	],
 	port: 3000,
 });
