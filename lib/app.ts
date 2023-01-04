14455bc62f6e7a94a98846da31f9c29616001d2d
notangelmario <savin@angelmario.eu>
2023-01-05 00:50:54 +0200

    Switched to semver versioning

--- lib/app.ts
+++ lib/app.ts
@@ -2,7 +2,7 @@ import "dotenv";
 import { exec } from "https://deno.land/x/execute@v1.1.0/mod.ts";
 
 export const APP = {
-	version: await exec("git rev-parse --short HEAD"),
+	version: "bf3e778",
 	codename: "Bianca",
 };
 
