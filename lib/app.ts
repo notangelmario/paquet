bf3e7789498d97d574ef6fda4de4fe62d5348814
notangelmario <savin@angelmario.eu>
2023-01-05 00:47:16 +0200

    Empty

--- lib/app.ts
+++ lib/app.ts
@@ -1,8 +1,6 @@
 import "dotenv";
 import { exec } from "https://deno.land/x/execute@v1.1.0/mod.ts";
 
-
-
 export const APP = {
 	version: await exec("git rev-parse --short HEAD"),
 	codename: "Bianca",
