29cb85383003414e4cfb5b0363cfa0f438d511ef
notangelmario <savin@angelmario.eu>
2023-01-24 00:16:39 +0200

    Fixed service worker error

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+const OFFLINE_URL = 'offline';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
