e61c3c8e446c874d00103e019db29153c43c20f7
Savin Angel-Mario <savin@angelmario.eu>
2023-06-28 01:27:17 +0300

    Added redirect notice in offline page

--- routes/offline.tsx
+++ routes/offline.tsx
@@ -1,8 +1,10 @@
 import { Head } from "$fresh/runtime.ts";
 import Header from "@/components/Header.tsx";
 import Container from "@/components/Container.tsx";
+import { PageProps } from "$fresh/server.ts";
+import Card from "../components/Card.tsx";
 
-export default function Offline() {
+export default function Offline(props: PageProps) {
 	return (
 		<>
 			<Head>
@@ -17,6 +19,13 @@ export default function Offline() {
 				<p>
 					You are offline. Please check your internet connection.
 				</p>
+				{props.url.hostname !== "paquet.shop" && (
+					<Card>
+						<p class="text-2xl">
+							We moved to <a class="underline text-primary" href="https://paquet.app">paquet.app</a>.
+						</p>
+					</Card>
+				)}
 			</Container>
 		</>
 	);
