53bdab21e27ed396b8bb6cada6d28d98d42c2e29
Savin Angel-Mario <savin@angelmario.eu>
2024-03-24 14:31:43 +0200

    Fixed production builds

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,17 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+import "dotenv";
 import { start } from "$fresh/server.ts";
 import manifest from "@/fresh.gen.ts";
-
-import "dotenv";
-
 import { checkUpdates } from "../scripts/update.ts";
 import { DEV } from "@/lib/app.ts";
-import tailwind from "$fresh/plugins/tailwind.ts";
+import { createQueueValueHandler } from "deno-kv-insights";
 
-import { kvInsightsPlugin, createQueueValueHandler } from "deno-kv-insights";
+import config from "@/fresh.config.ts";
 
-import { freshSEOPlugin } from "fresh-seo";
-
-const kvInsightsQueueValueHandler = createQueueValueHandler();
 const kv = await Deno.openKv();
-const docs = Deno.readDirSync("docs");
-
+const kvInsightsQueueValueHandler = createQueueValueHandler();
 kv.listenQueue(async (value: unknown) => {
   await kvInsightsQueueValueHandler(value);
 });
@@ -32,16 +26,4 @@ if (!DEV) {
 }
 
 // @ts-ignore: I don't care about the type of the manifest
-await start(manifest, {
-	plugins: [
-		tailwind(),
-		kvInsightsPlugin({ kv }),
-		freshSEOPlugin(manifest, {
-			include: [
-				...Array.from(docs).map((doc) => `/docs/${doc.name}`),
-			],
-			exclude: ["/app/error", "/api/**", "/gfm.css", "/offline"],
-		}),
-	],
-	port: parseInt(Deno.env.get("PORT") || "3000"),
-});
+await start(manifest, config);
